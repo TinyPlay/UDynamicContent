019dad5d67674bbe1dc2cdc15780b0694562f325
TinyPlay <ceo@tpgames.ru>
2021-12-28 23:59:35 +0500

    Update Core

---
+++ Dynamic Content/Assets/UDynamicContent/DynamicContent.cs
@@ -0,0 +1,33 @@
+/*
+ * Unity Dynamic Content Library
+ * This library is designed to dynamically load content in
+ * the game on the fly.
+ *
+ * With it you can organize the loading of better
+ * graphics and other components.
+ *
+ * @developer           Ilya Rastorguev
+ * @version             0.7.2
+ * @url                 https://github.com/TinyPlay/UDynamicContent/
+ * @author              TinyPlay, Inc.
+ * @license             MIT
+ */
+namespace UDynamicContent{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Managers;
+    
+    /// <summary>
+    /// Dynamic Content Wrapper Class
+    /// </summary>
+    public class DynamicContent
+    {
+        
+        // Internal Managers
+        private QueueManager _queue;
+        private DownloadManager _downloader;
+        
+        
+    }
+}
---
+++ Dynamic Content/Assets/UDynamicContent/Managers/DownloadManager.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Unity Dynamic Content Library
+ * This library is designed to dynamically load content in
+ * the game on the fly.
+ *
+ * With it you can organize the loading of better
+ * graphics and other components.
+ *
+ * @developer           Ilya Rastorguev
+ * @version             0.7.2
+ * @url                 https://github.com/TinyPlay/UDynamicContent/
+ * @author              TinyPlay, Inc.
+ * @license             MIT
+ */
+namespace UDynamicContent.Managers
+{
+    /// <summary>
+    /// Download Manager
+    /// </summary>
+    internal class DownloadManager
+    {
+        
+    }
+}
